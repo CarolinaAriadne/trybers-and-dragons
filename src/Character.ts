b3cc1e1a5c4d7fa40639d2bd372cbaca7b34ec83
Carolina Ariadne <c.ariadneadv@gmail.com>
2022-06-27 23:03:26 -0300

    Requisito-7-inicio

---
+++ src/Character.ts
@@ -0,0 +1,22 @@
+// import Archetype from './Archetypes';
+// import Energy from './Energy';
+// import Fighter from './Fighter';
+// import Race, { Elf } from './Races';
+
+// class Character implements Fighter {
+//   private _race: Race;
+//   private archetype: Archetype;
+//   private maxLifePoints: number;
+//   private lifePoints: number;
+//   private strenght: number;
+//   private defense: number;
+//   private _dexterity: number;
+//   private energy: Energy;
+
+//   constructor(dexterity: number) {
+//     this._dexterity = Math.random() * 10 + 1;
+//     this._race = 
+//   }
+// }
+
+// export default Character;
